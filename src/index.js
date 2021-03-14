01b604624f93aa243140320065a78765e872f890
Yurii Popsui <yp.onward@gmail.com>
2021-03-14 16:44:58 +0200

    init: start Reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+
+  if(n < 0){
+    n *= (-1);
+    return n.toString().split('').reverse().join('');
+  }
+  return n.toString().split('').reverse().join('');
 }
